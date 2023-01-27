8b3adeabe98c9afc6c051c9595e892e2d4124da2
Konrad Wasilewski <konradwasilewski@vp.pl>
2023-01-27 14:32:39 +0100

    Commit before going to work

--- app/src/main/java/com/example/learnbyrepetition/ui/flashcards/FlashcardAdapter.kt
+++ app/src/main/java/com/example/learnbyrepetition/ui/flashcards/FlashcardAdapter.kt
@@ -1,14 +1,14 @@
-package com.example.learnbyrepetition
+package com.example.learnbyrepetition.ui.flashcards
 
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.learnbyrepetition.R
 import com.example.learnbyrepetition.database.classes.Flashcard
 import com.example.learnbyrepetition.newActivities.DetailsFlashcardActivity
 import java.text.SimpleDateFormat
